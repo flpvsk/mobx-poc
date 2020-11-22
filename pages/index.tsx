4f0b8e5f89f5e9b433163f03ee2902dd36f7ddf6
Andrey Salomatin <filipovskii.off@gmail.com>
2020-11-22 23:56:19 +0100

    Init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,51 @@
-import SampleComponent from '../components/SampleComponent'
+import React, { useState } from 'react';
+import { observer } from 'mobx-react-lite';
+import { useRouter } from 'next/router';
+import { useStore } from '../store';
 
-export default function Home() {
-  return <SampleComponent title="Index Page" linkTo="/other" />
-}
+const Home = observer(() => {
+  const store = useStore();
+  const router = useRouter();
+  const [q, setQ] = useState('');
+
+  function onSearchChange(e) {
+    setQ(e.target.value);
+  }
+
+  function searchForSuggestions(e: React.FormEvent) {
+    e.preventDefault();
+    store.fetchLocationSuggestions(q);
+  }
+
+  function chooseLocation(location) {
+    return (e) => {
+      e.preventDefault();
+      const cartId = store.startCheckout(location.id);
+      router.push({
+        pathname: '/cart/[cartId]',
+        query: { cartId },
+      });
+    };
+  }
+
+  const locationList = store.locationSuggestions.map((location) => (
+    <li key={location.id}>
+      <a href={'#'} onClick={chooseLocation(location)}>
+        {location.city}, {location.postalCode}
+      </a>
+    </li>
+  ));
+
+  return (
+    <main>
+      <h3>Search for an address</h3>
+      <form onSubmit={searchForSuggestions}>
+        <input type="text" value={q} onChange={onSearchChange} />
+        <input type="submit" value="search" />
+      </form>
+      <ul>{locationList}</ul>
+    </main>
+  );
+});
+
+export default Home;
--- store.ts
+++ store.ts
@@ -1,61 +1,150 @@
-import { useMemo } from 'react'
+import { useMemo } from 'react';
+import { nanoid } from 'nanoid';
 import {
   applySnapshot,
   Instance,
   SnapshotIn,
   SnapshotOut,
+  getParent,
+  flow,
   types,
-} from 'mobx-state-tree'
+  IArrayType,
+  IAnyType,
+} from 'mobx-state-tree';
 
-let store: IStore | undefined
+import { Server } from './server';
+
+let store: IStore | undefined;
+
+const LocationInfo = types.model('LocationInfo', {
+  id: types.identifier,
+  lat: types.number,
+  lng: types.number,
+  postalCode: types.string,
+  city: types.string,
+});
+
+type ILocationInfo = Instance<typeof LocationInfo>;
+
+const Product = types.model({
+  id: types.identifier,
+  name: types.string,
+  priceValue: types.number,
+  currencyCode: types.string,
+  currencySymbol: types.string,
+});
+
+type IProduct = Instance<typeof Product>;
+
+const CartItem = types
+  .model({
+    product: types.reference(Product),
+    quantity: types.number,
+  })
+  .actions((self) => ({
+    setQuantity(q) {
+      self.quantity = q;
+    },
+    remove() {
+      getParent<IArrayType<IAnyType>>(self).remove(self);
+    },
+  }));
+
+type ICartItem = Instance<typeof CartItem>;
+
+const Cart = types.model({
+  id: types.identifier,
+  currentStep: types.number,
+  locationInfo: types.maybe(types.reference(LocationInfo)),
+  cartItems: types.array(CartItem),
+});
+
+type ICart = Instance<typeof Cart>;
 
 const Store = types
   .model({
-    lastUpdate: types.Date,
-    light: false,
+    cartMap: types.map(Cart),
+    locationSuggestions: types.array(LocationInfo),
+    lastFetchedProducts: types.optional(types.number, 0),
+    isFetchingProducts: types.optional(types.boolean, false),
+    productsAvailable: types.array(Product),
   })
   .actions((self) => {
-    let timer: any
-    const start = () => {
-      timer = setInterval(() => {
-        // mobx-state-tree doesn't allow anonymous callbacks changing data.
-        // Pass off to another action instead (need to cast self as any
-        // because typescript doesn't yet know about the actions we're
-        // adding to self here)
-        ;(self as any).update()
-      }, 1000)
-    }
-    const update = () => {
-      self.lastUpdate = new Date(Date.now())
-      self.light = true
-    }
-    const stop = () => {
-      clearInterval(timer)
-    }
-    return { start, stop, update }
-  })
+    const fetchLocationSuggestions = flow(function* fetchSuggestions(
+      s: string,
+    ) {
+      const locationInfoList = yield Server.fetchLocationSuggestions(
+        s,
+      );
+      self.locationSuggestions = locationInfoList;
+    });
 
-export type IStore = Instance<typeof Store>
-export type IStoreSnapshotIn = SnapshotIn<typeof Store>
-export type IStoreSnapshotOut = SnapshotOut<typeof Store>
+    const startCart = (locationId: string) => {
+      let location: ILocationInfo;
+      for (let l of self.locationSuggestions) {
+        if (l.id === locationId) {
+          location = l;
+        }
+      }
+      const cartId = nanoid();
+      self.cartMap.put({
+        id: cartId,
+        locationInfo: location,
+        currentStep: 0,
+        cartItems: [],
+      } as ICart);
+
+      return cartId;
+    };
+
+    const fetchProducts = flow(function* fetchProductsFlow() {
+      self.isFetchingProducts = true;
+      const products = yield Server.fetchProducts();
+      self.productsAvailable = products.map((p) => ({
+        id: p.id,
+        name: p.name,
+        priceValue: p.price.value,
+        currencyCode: p.price.currency.code,
+        currencySymbol: p.price.currency.symbol,
+      }));
+      self.lastFetchedProducts = Date.now();
+      self.isFetchingProducts = false;
+    });
+
+    return {
+      fetchLocationSuggestions,
+      startCheckout: startCart,
+      fetchProducts,
+    };
+  });
+
+export type IStore = Instance<typeof Store>;
+export type IStoreSnapshotIn = SnapshotIn<typeof Store>;
+export type IStoreSnapshotOut = SnapshotOut<typeof Store>;
 
 export function initializeStore(snapshot = null) {
-  const _store = store ?? Store.create({ lastUpdate: 0 })
+  const _store =
+    store ??
+    Store.create({
+      cartMap: {},
+      locationSuggestions: [],
+    });
 
-  // If your page has Next.js data fetching methods that use a Mobx store, it will
-  // get hydrated here, check `pages/ssg.tsx` and `pages/ssr.tsx` for more details
+  // If your page has Next.js data fetching methods that use a Mobx store,
+  // it will get hydrated here, check `pages/ssg.tsx` and `pages/ssr.tsx`
+  // for more details
   if (snapshot) {
-    applySnapshot(_store, snapshot)
+    applySnapshot(_store, snapshot);
   }
   // For SSG and SSR always create a new store
-  if (typeof window === 'undefined') return _store
+  if (typeof window === 'undefined') return _store;
   // Create the store once in the client
-  if (!store) store = _store
+  if (!store) store = _store;
 
-  return store
+  return store;
 }
 
-export function useStore(initialState: any) {
-  const store = useMemo(() => initializeStore(initialState), [initialState])
-  return store
+export function useStore(): IStore {
+  const store = useMemo<IStore>(() => initializeStore(null), []);
+  return store;
 }
